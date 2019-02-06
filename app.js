a62373172f44e04e74921dddb37adc314f0da025
nishant <happysoni777@gmail.com>
2019-02-06 20:11:21 +0530

    Homepage screen1

---
+++ app.js
@@ -0,0 +1,17 @@
+const express=require('express');
+const app=express();
+const exphbs=require('express-handlebars');
+//setting handlebars
+app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
+
+//servin static files
+app.use(express.static('img'));
+app.use(express.static('css'));
+
+app.get('/', function (req, res) {
+    res.render('index');
+});
+
+//listening on port 5000
+app.listen(5000);
